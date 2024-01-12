69548ef47ed0a089221e5d178901f832b3cc9710
siddhant chettri <siddhantchtri@gmail.com>
2024-01-12 15:35:21 +0530

    error fix

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import "./App.css";
-import { HashRouter } from "react-router-dom";
-import { Route, RouterProvider, Routes } from "react-router-dom";
+import React from "react";
+import {
+  createBrowserRouter,
+  Link,
+  Route,
+  RouterProvider,
+  Routes,
+} from "react-router-dom";
 import Home from "./pages/collage/home/Home";
 import Login from "./pages/login/Login";
 import CompanyPage from "./pages/collage/home/CompanyPage";
@@ -24,69 +30,74 @@ import TeamsPage from "./pages/collage/teams/TeamsPage";
 import CompaniesPage from "./pages/collage/companies/CompaniesPage";
 import StudentsMainPage from "./pages/collage/students/StudentsMainPage";
 
+// 3️⃣ Router singleton created
+const router = createBrowserRouter([{ path: "*", Component: Root }]);
+
 export default function App() {
+  return <RouterProvider router={router} />;
+}
+
+function Root() {
   // 2️⃣ `BrowserRouter` component removed, but the <Routes>/<Route>
   // component below are unchanged
   return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <Routes>
+      <Route path="" element={<Home />} />
 
-        <Route path="collage/dashboard">
-          <Route path="" element={<Home />} />
-          <Route path="companies" element={<CompanyPage />} />
-          <Route path="jobs" element={<JobsPage />} />
-          <Route path="students" element={<StudentsPage />} />
-        </Route>
+      <Route path="collage/dashboard">
+        <Route path="" element={<Home />} />
+        <Route path="companies" element={<CompanyPage />} />
+        <Route path="jobs" element={<JobsPage />} />
+        <Route path="students" element={<StudentsPage />} />
+      </Route>
 
-        <Route path="collage/test">
-          <Route path="" element={<TestPage />} />
-          <Route path="assessment" element={<AssessmentPage />} />
-          <Route path="name" element={<NameAssessmentPage />} />
-          <Route path="select" element={<SelectTestsPage />} />
-          <Route path="questions" element={<AddQuestionsPage />} />
-          <Route path="review" element={<ReviewPage />} />
-          <Route path="addMcq" element={<AddMcqPage />} />
-        </Route>
+      <Route path="collage/test">
+        <Route path="" element={<TestPage />} />
+        <Route path="assessment" element={<AssessmentPage />} />
+        <Route path="name" element={<NameAssessmentPage />} />
+        <Route path="select" element={<SelectTestsPage />} />
+        <Route path="questions" element={<AddQuestionsPage />} />
+        <Route path="review" element={<ReviewPage />} />
+        <Route path="addMcq" element={<AddMcqPage />} />
+      </Route>
 
-        <Route path="collage/accounting">
-          <Route path="" element={<AccountingPage />} />
-        </Route>
+      <Route path="collage/accounting">
+        <Route path="" element={<AccountingPage />} />
+      </Route>
 
-        <Route path="collage/companies">
-          <Route path="" element={<CompaniesPage />} />
-        </Route>
+      <Route path="collage/companies">
+        <Route path="" element={<CompaniesPage />} />
+      </Route>
 
-        <Route path="collage/inbox">
-          <Route path="" element={<InboxPage />} />
-        </Route>
+      <Route path="collage/inbox">
+        <Route path="" element={<InboxPage />} />
+      </Route>
 
-        <Route path="collage/profile">
-          <Route path="" element={<ProfilePage />} />
-        </Route>
+      <Route path="collage/profile">
+        <Route path="" element={<ProfilePage />} />
+      </Route>
 
-        <Route path="collage/quesBank">
-          <Route path="" element={<QuesBankPage />} />
-        </Route>
+      <Route path="collage/quesBank">
+        <Route path="" element={<QuesBankPage />} />
+      </Route>
 
-        <Route path="collage/results">
-          <Route path="" element={<ResultsPage />} />
-        </Route>
+      <Route path="collage/results">
+        <Route path="" element={<ResultsPage />} />
+      </Route>
 
-        <Route path="collage/settings">
-          <Route path="" element={<SettingsPage />} />
-        </Route>
+      <Route path="collage/settings">
+        <Route path="" element={<SettingsPage />} />
+      </Route>
 
-        <Route path="collage/students">
-          <Route path="" element={<StudentsMainPage />} />
-        </Route>
+      <Route path="collage/students">
+        <Route path="" element={<StudentsMainPage />} />
+      </Route>
 
-        <Route path="collage/teams">
-          <Route path="" element={<TeamsPage />} />
-        </Route>
+      <Route path="collage/teams">
+        <Route path="" element={<TeamsPage />} />
+      </Route>
 
-        <Route path="login" element={<Login />} />
-      </Routes>
-    </HashRouter>
+      <Route path="login" element={<Login />} />
+    </Routes>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,14 +4,12 @@ import "./index.css";
 import App from "./App";
 import store from "./redux/store";
 import { Provider } from "react-redux";
-import { HashRouter } from "react-router-dom";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <App />
     </Provider>
   </React.StrictMode>
 );
