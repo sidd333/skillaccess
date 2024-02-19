39dd5ea64e3e87f72e1c44c55566685f110874ad
sidd333 <siddhantchtri@gmail.com>
2024-02-19 18:14:16 +0530

    profile fix

--- src/components/collage/profile/home/Profile.jsx
+++ src/components/collage/profile/home/Profile.jsx
@@ -12,98 +12,97 @@ import {
 
 const Profile = () => {
   // <<<<<<< AnkitaMalik22-ankita-dev
-    const dispatch = useDispatch();
-    const { user,isLoggedIn ,uploadImg} = useSelector((state) => state.collageAuth);
-    const [editable, setEditable] = useState(false);
+  const dispatch = useDispatch();
+  const { user, isLoggedIn, uploadImg } = useSelector(
+    (state) => state.collageAuth
+  );
+  const [editable, setEditable] = useState(false);
 
-    const [avatar, setAvatar] = useState("");
+  const [avatar, setAvatar] = useState("");
 
-    const [college, setCollege] = useState({
-      CollegeName: user ? user.CollegeName : "",
-      Email: user ? user.Email : "",
-      Phone: user ? user.Phone : "",
-      Address: user ? user.Address : "",
-      Website: user ? user.Website : "",
-      Phone : user ? user.Phone : "",
-      avatar:{
-        public_id: user  && user.avatar ? user.avatar.public_id : "",
-        url: user && user.avatar ? user.avatar.url : "",
+  const [college, setCollege] = useState({
+    CollegeName: user ? user.CollegeName : "",
+    Email: user ? user.Email : "",
+    Phone: user ? user.Phone : "",
+    Address: user ? user.Address : "",
+    Website: user ? user.Website : "",
+    Phone: user ? user.Phone : "",
+    avatar: {
+      public_id: user && user.avatar ? user.avatar.public_id || "" : "",
+      url: user && user.avatar ? user.avatar.url : "",
+    },
+    Description: user ? user.Description : "",
+    Code: user ? user.code : "",
+    Location: user ? user.Location : "",
+    State: user ? user.State : "",
+    City: user ? user.City : "",
+  });
 
-      },
-      Description: user ? user.Description : "",
-      Code: user ? user.code : "",
-      Location: user ? user.Location : "",
-      State: user ? user.State : "",
-      City: user ? user.City : "",
-    });
+  const handleUpdate = (college) => {
+    console.log("update");
+    dispatch(updateCollege(college));
+    setEditable(false);
+  };
 
+  useEffect(() => {
+    if (user) {
+      setCollege({
+        CollegeName: user.CollegeName,
+        Email: user.Email,
+        Phone: user.Phone,
+        Address: user.Address,
+        Website: user.Website,
+        avatar: {
+          public_id: user.avatar.public_id || "",
+          url: user.avatar.url,
+        },
 
-
-    const handleUpdate = (college) => {
-      console.log("update");
-      dispatch(updateCollege(college));
-      setEditable(false);
-    }
-
-    useEffect(() => {
-      if (user) {
-        setCollege({
-          CollegeName: user.CollegeName,
-          Email: user.Email,
-          Phone: user.Phone,
-          Address: user.Address,
-          Website: user.Website,
-          avatar: {
-            public_id: user.avatar.public_id,
-            url:  user.avatar.url,
-          },
-          
-          Description: user.Description,
-          Code: user.code,
-          Location: user.Location,
-          State: user.State,
-          City: user.City,
-        });
-        dispatch(getCollege());
-      }
-    }, [dispatch,isLoggedIn]);
-
-    useEffect(() => {
+        Description: user.Description,
+        Code: user.code,
+        Location: user.Location,
+        State: user.State,
+        City: user.City,
+      });
       dispatch(getCollege());
-      // console.log(college, "college");
-    }, [dispatch]);
+    }
+  }, [dispatch, isLoggedIn]);
 
-    useEffect(() => {
+  useEffect(() => {
+    dispatch(getCollege());
+    // console.log(college, "college");
+  }, [dispatch]);
 
-     
-      if (user && user.avatar && user.avatar.url) {
-        setAvatar(user.avatar.url);
-      }
-      if (avatar) {
-        dispatch(updateAvatar({ avatar }));
-        dispatch(getCollege());
-        // setCollege({ ...college, avatar: { url:user.avatar.url} });
-      }
-      dispatch(updateCollege(college));
-          dispatch(getCollege());
-  ;
-    }, [editable]);
+  useEffect(() => {
+    if (user && user.avatar && user.avatar.url) {
+      setAvatar(user.avatar.url);
+    }
+    if (avatar) {
+      dispatch(updateAvatar({ avatar }));
+      dispatch(getCollege());
+      // setCollege({ ...college, avatar: { url:user.avatar.url} });
+    }
+    dispatch(updateCollege(college));
+    dispatch(getCollege());
+  }, [editable]);
 
-
-
-    // useEffect(() => {
-    //   dispatch(getCollege());
-    //   dispatch(setUploadImg(false))
-    // }, [uploadImg]);
+  // useEffect(() => {
+  //   dispatch(getCollege());
+  //   dispatch(setUploadImg(false))
+  // }, [uploadImg]);
   // =======
   // const [editable, setEditable] = useState(false);
 
   return (
     <div className="px-8">
-      {editable && <EditHeader editable={editable} setEditable={setEditable}   handleUpdate={handleUpdate}
-      college={college}
-      setCollege={setCollege}
-      />}
+      {editable && (
+        <EditHeader
+          editable={editable}
+          setEditable={setEditable}
+          handleUpdate={handleUpdate}
+          college={college}
+          setCollege={setCollege}
+        />
+      )}
       {/* {editable && <EditHeader editable={editable} setEditable={setEditable} />} */}
       <Header
         editable={editable}
