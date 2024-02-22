7c314ba8cd4d5643b337e68e91b9f9b567a337fe
sidd333 <siddhantchtri@gmail.com>
2024-02-22 09:58:59 +0530

    prof fixed finalll

--- src/components/collage/profile/home/Profile.jsx
+++ src/components/collage/profile/home/Profile.jsx
@@ -1,130 +1,3 @@
-// import React, { useEffect, useState } from "react";
-// import Header from "./Header";
-// import List from "./List";
-// import EditHeader from "./EditHeader";
-// import { useDispatch, useSelector } from "react-redux";
-// import {
-//   getCollege,
-//   updateCollege,
-//   updateAvatar,
-//   setUploadImg,
-// } from "../../../../redux/collage/auth/authSlice";
-
-// const Profile = () => {
-//   // <<<<<<< AnkitaMalik22-ankita-dev
-//   const dispatch = useDispatch();
-//   const { user, isLoggedIn, uploadImg } = useSelector(
-//     (state) => state.collageAuth
-//   );
-//   const [editable, setEditable] = useState(false);
-
-//   const [avatar, setAvatar] = useState("");
-
-//   const [college, setCollege] = useState(user);
-
-//   const handleUpdate = (college) => {
-//     console.log("update");
-//     dispatch(updateCollege(college));
-//     setEditable(false);
-//   };
-
-//   useEffect(() => {
-//     if (user) {
-//       setCollege(user);
-//       dispatch(getCollege());
-//     }
-//   }, [dispatch, isLoggedIn]);
-
-//   useEffect(() => {
-//     // if (!user){
-//     dispatch(getCollege());
-
-//     console.log(college);
-//     // }
-//     // console.log(college, "college");
-//   }, []);
-//   useEffect(() => {
-//     if (user) {
-//       setCollege(user);
-//     } else {
-//       dispatch(getCollege());
-//     }
-//   }, [user]);
-
-//   //     dispatch(getCollege()).then((res) =>
-//   //       setCollege({
-//   //         CollegeName: res.payload.CollegeName || "college name ",
-//   //         Email: res.payload.Email || "email",
-//   //         Phone: res.payload.Phone || "phone",
-//   //         Address: res.payload.Address || "address",
-//   //         Website: res.payload.Website || "web",
-//   //         avatar: {
-//   //           public_id: res.payload.avatar?.public_id || "",
-//   //           url: res.payload.avatar?.url || "",
-//   //         },
-
-//   //         Description: res.payload.Description || "desc",
-//   //         Code: res.payload.code || "code",
-//   //         Location: res.payload.Location || "loc",
-//   //         State: res.payload.State || "state",
-//   //         City: res.payload.City || "city",
-//   //       })
-//   //     );
-
-//   //     console.log(user);
-//   //   }, []);
-
-//   useEffect(() => {
-//     if (user && user.avatar && user.avatar.url) {
-//       setAvatar(user.avatar.url);
-//     }
-//     if (avatar) {
-//       dispatch(updateAvatar({ avatar }));
-//       dispatch(getCollege());
-//       // setCollege({ ...college, avatar: { url:user.avatar.url} });
-//     }
-//     // dispatch(updateCollege(college));
-//     dispatch(getCollege());
-//   }, [editable]);
-
-//   // useEffect(() => {
-//   //   dispatch(getCollege());
-//   //   dispatch(setUploadImg(false))
-//   // }, [uploadImg]);
-//   // =======
-//   // const [editable, setEditable] = useState(false);
-
-//   return (
-//     <div className="px-8">
-//       {user && editable && (
-//         <EditHeader
-//           editable={editable}
-//           setEditable={setEditable}
-//           handleUpdate={handleUpdate}
-//           college={college}
-//           setCollege={setCollege}
-//         />
-//       )}
-
-//       {user && (
-//         <Header
-//           editable={editable}
-//           setEditable={setEditable}
-//           handleUpdate={handleUpdate}
-//           college={college}
-//           setCollege={setCollege}
-//           setAvatar={setAvatar}
-//           avatar={avatar}
-//         />
-//       )}
-//       {!user && <h1> Loading ... </h1>}
-
-//       <List editable={editable} setEditable={setEditable} />
-//       {/* {window.location.reload(true)} */}
-//     </div>
-//   );
-// };
-
 import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import List from "./List";
@@ -143,6 +16,14 @@ const Profile = () => {
     (state) => state.collageAuth
   );
   const [editable, setEditable] = useState(false);
+
+  useEffect(() => {
+    const value = localStorage.getItem("editable");
+    if (value === "true") {
+      setEditable(true);
+    }
+  }, []);
+
   const [submitUpdateProfile, setSubmitUpdateProfile] = useState(false);
   const [avatar, setAvatar] = useState("");
   const [college, setCollege] = useState(user);
@@ -150,6 +31,7 @@ const Profile = () => {
     console.log("update");
     dispatch(updateCollege(college));
     setEditable(false);
+    localStorage.setItem("editable", false);
   };
   useEffect(() => {
     if (user) {
@@ -210,6 +92,7 @@ const Profile = () => {
       dispatch(getCollege());
       setSubmitUpdateProfile(false);
       setEditable(false);
+      localStorage.setItem("editable", false);
     }
   }, [submitUpdateProfile]);
   // useEffect(() => {
--- src/redux/collage/auth/authSlice.js
+++ src/redux/collage/auth/authSlice.js
@@ -1,321 +1,3 @@
-// import { createSlice } from "@reduxjs/toolkit";
-// import { createAsyncThunk } from "@reduxjs/toolkit";
-// import axios from "axios";
-
-// const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
-
-// console.log(process.env);
-
-// //initial state
-
-// const collageState = {
-//   status: "",
-//   error: "",
-//   isLoggedIn: false,
-//   user: null,
-//   uploadImg: false,
-// };
-
-// const authToken = localStorage.getItem("auth-token");
-
-// export const registerCollage = createAsyncThunk(
-//   "collageAuth/registerCollage",
-//   async (data, { rejectWithValue }) => {
-//     try {
-//       console.log("registering");
-//       console.log(process.env);
-//       const req = await axios.post(
-//         `${REACT_APP_API_URL}/api/college/register`,
-//         data,
-//         { withCredentials: true }
-//       );
-//       const res = req.data;
-//       localStorage.setItem("auth-token", res.token);
-//       return res.data;
-//     } catch (error) {
-//       console.log("catch");
-//       return rejectWithValue(error.response.data.message);
-//     }
-//   }
-// );
-// export const loginCollage = createAsyncThunk(
-//   "collageAuth/loginCollage",
-//   async (data, { rejectWithValue }) => {
-//     try {
-//       console.log("login");
-//       const req = await axios.post(
-//         `${REACT_APP_API_URL}/api/college/login`,
-//         data,
-//         { withCredentials: true }
-//       );
-//       const res = req.data;
-//       localStorage.setItem("auth-token", res.token);
-//       console.log(res);
-//       return res;
-//     } catch (error) {
-//       console.log("catch");
-//       return rejectWithValue(error.response.data.message);
-//     }
-//   }
-// );
-
-// export const updateCollege = createAsyncThunk(
-//   "collageAuth/updateCollege",
-//   async (data, { rejectWithValue }) => {
-//     try {
-//       console.log("updating", authToken);
-//       const req = await axios.put(
-//         `${REACT_APP_API_URL}/api/college/update`,
-//         data,
-
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//             "auth-token": authToken,
-//           },
-//         }
-//       );
-
-//       const res = req.data;
-//       console.log("should not reject");
-//       console.log(res);
-//       return res.data.college;
-//     } catch (error) {
-//       console.log("catch", error.response.data);
-//       return rejectWithValue(error.response.data);
-//     }
-//   }
-// );
-
-// export const getCollege = createAsyncThunk(
-//   "collageAuth/getCollege",
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       const response = await axios.get(`${REACT_APP_API_URL}/api/college/me`, {
-//         withCredentials: true,
-//         headers: {
-//           "auth-token": localStorage.getItem("auth-token"),
-//         },
-//       });
-
-//       return response.data.college;
-//     } catch (error) {
-//       return rejectWithValue(error.response.data);
-//     }
-//   }
-// );
-
-// export const updateAvatar = createAsyncThunk(
-//   "collageAuth/updateAvatar",
-//   async (data, { rejectWithValue }) => {
-//     try {
-//       console.log("updating", authToken);
-//       const req = await axios.put(
-//         `${REACT_APP_API_URL}/api/college/update/avatar`,
-//         data,
-
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//             "auth-token": authToken,
-//           },
-//         }
-//       );
-//       const res = req.data;
-//       return res.data.college;
-//     } catch (error) {
-//       console.log("catch", error.response.data);
-//       return rejectWithValue(error.response.data);
-//     }
-//   }
-// );
-
-// export const logoutCollage = createAsyncThunk(
-//   "collageAuth/logoutCollage",
-//   async (_, { rejectWithValue }) => {
-//     try {
-//       console.log("logout");
-//       const req = await axios.get(`${REACT_APP_API_URL}/api/college/logout`, {
-//         withCredentials: true,
-//       });
-//       const res = req.data;
-//       localStorage.removeItem("auth-token");
-//       return res.data;
-//     } catch (error) {
-//       console.log("catch");
-//       return rejectWithValue(error.response.data);
-//     }
-//   }
-// );
-
-// export const updatePassword = createAsyncThunk(
-//   "collageAuth/updatePassword",
-
-//   async (data, { rejectWithValue }) => {
-//     try {
-//       console.log("updating", authToken);
-//       const req = await axios.put(
-//         `${REACT_APP_API_URL}/api/college/password/update`,
-//         data,
-
-//         {
-//           headers: {
-//             "Content-Type": "application/json",
-//             "auth-token": authToken,
-//           },
-//         }
-//       );
-//       const res = req.data;
-//       return res.data;
-//     } catch (error) {
-//       console.log("catch", error.response.data);
-//       return rejectWithValue(error.response.data);
-//     }
-//   }
-// );
-
-// const collageAuthSlice = createSlice({
-//   name: "collageAuth",
-//   initialState: collageState,
-//   reducers: {
-//     setUploadImg: (state, action) => {
-//       state.uploadImg = action.payload;
-//     },
-//   },
-//   extraReducers: (builder) => {
-//     builder
-//       .addCase(registerCollage.pending, (state, action) => {
-//         state.status = "loading";
-//         console.log("pending");
-//       })
-//       .addCase(registerCollage.fulfilled, (state, action) => {
-//         // state.status = action.payload
-//         state.isLoggedIn = true;
-//         state.user = action.payload;
-//         // Add any fetched posts to the array
-//         console.log("fullfilled");
-//       })
-//       .addCase(registerCollage.rejected, (state, action) => {
-//         console.log(action.payload);
-
-//         // <<<<<<< AnkitaMalik22-ankita-dev
-//         //   alert(action.payload);
-//         // =======
-
-//         window.alert(action.payload || "invalid credentials");
-//       })
-//       .addCase(loginCollage.pending, (state, action) => {
-//         state.status = "loading";
-//         console.log("pending");
-//       })
-//       .addCase(loginCollage.fulfilled, (state, action) => {
-//         // state.status = action.payload
-
-//         state.isLoggedIn = true;
-//         state.user = action.payload;
-
-//         console.log(state.user);
-//       })
-//       .addCase(loginCollage.rejected, (state, action) => {
-//         alert(action.payload);
-//       })
-//       .addCase(updateCollege.pending, (state, action) => {
-//         state.status = "loading";
-//         console.log("pending");
-//       })
-//       .addCase(updateCollege.fulfilled, (state, action) => {
-//         // state.status = action.payload
-//         state.user = action.payload;
-//         window.location.reload(true);
-//         console.log("update college fulfilled");
-//       })
-//       .addCase(updateCollege.rejected, (state, action) => {
-//         // console.log(action.payload);
-//         // window.alert(action.payload);
-//         // window.location.reload(true);
-//         console.log("rejected update profile");
-//       })
-//       .addCase(getCollege.pending, (state, action) => {
-//         state.status = "loading";
-//         console.log("pending");
-//       })
-//       .addCase(getCollege.fulfilled, (state, action) => {
-//         // state.status = action.payload
-//         state.isLoggedIn = true;
-//         state.user = action.payload;
-
-//         // Add any fetched posts to the array
-//         console.log("fullfilled get college");
-//       })
-//       .addCase(getCollege.rejected, (state, action) => {
-//         console.log(action.payload);
-
-//         // window.alert(action.payload);
-//       })
-//       .addCase(updateAvatar.pending, (state, action) => {
-//         state.status = "loading";
-//         console.log("pending avatar");
-//       })
-//       .addCase(updateAvatar.fulfilled, (state, action) => {
-//         // state.status = action.payload
-
-//         state.status = "success";
-//         state.user = action.payload;
-//         state.uploadImg = true;
-//         // state.user = action.payload;
-
-//         // getCollege();
-//         // Add any fetched posts to the array
-//         console.log("fullfilled avatar");
-//       })
-//       .addCase(updateAvatar.rejected, (state, action) => {
-//         console.log("rejected avatar" + action.payload);
-
-//         // window.alert(action.payload);
-//       })
-//       .addCase(logoutCollage.pending, (state, action) => {
-//         state.status = "loading";
-//         console.log("pending");
-//       })
-//       .addCase(logoutCollage.fulfilled, (state, action) => {
-//         // state.status = action.payload
-
-//         state.user = null;
-//         state.isLoggedIn = false;
-
-//         // Add any fetched posts to the array
-//         console.log("fullfilled");
-//       })
-//       .addCase(logoutCollage.rejected, (state, action) => {
-//         console.log(action.payload);
-
-//         // window.alert(action.payload);
-//       })
-//       .addCase(updatePassword.pending, (state, action) => {
-//         state.status = "loading";
-//         console.log("pending");
-//       })
-//       .addCase(updatePassword.fulfilled, (state, action) => {
-//         state.status = action.payload;
-//         alert("Password Updated");
-//         // state.status = action.payload
-//         // state.isLoggedIn = false;
-//         // state.user = action.payload.user;
-//         // localStorage.removeItem("auth-token");
-
-//         // getCollege();
-//         // localStorage.setItem("auth-token", action.payload.token);
-//       })
-//       .addCase(updatePassword.rejected, (state, action) => {
-//         alert(action.payload.message);
-//       });
-//   },
-// });
-
-// //
-// export const { setUploadImg } = collageAuthSlice.actions;
-// export default collageAuthSlice.reducer;
-
 import { createSlice } from "@reduxjs/toolkit";
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
@@ -368,7 +50,8 @@ export const loginCollage = createAsyncThunk(
       );
       const res = req.data;
       localStorage.setItem("auth-token", res.token);
-      return res.data;
+      console.log(res);
+      return res;
     } catch (error) {
       console.log("catch");
       return rejectWithValue(error.response.data.message);
@@ -392,7 +75,10 @@ export const updateCollege = createAsyncThunk(
           },
         }
       );
+
       const res = req.data;
+      console.log("should not reject");
+      console.log(res);
       return res.data.college;
     } catch (error) {
       console.log("catch", error.response.data);
@@ -411,6 +97,7 @@ export const getCollege = createAsyncThunk(
           "auth-token": localStorage.getItem("auth-token"),
         },
       });
+
       return response.data.college;
     } catch (error) {
       return rejectWithValue(error.response.data);
@@ -435,9 +122,10 @@ export const updateAvatar = createAsyncThunk(
         }
       );
       const res = req.data;
-      return res.data.college;
+      // console.log(res);
+      return res;
     } catch (error) {
-      console.log("catch", error.response.data);
+      console.log(error);
       return rejectWithValue(error.response.data);
     }
   }
@@ -514,6 +202,7 @@ const collageAuthSlice = createSlice({
         // <<<<<<< AnkitaMalik22-ankita-dev
         //   alert(action.payload);
         // =======
+
         window.alert(action.payload || "invalid credentials");
       })
       .addCase(loginCollage.pending, (state, action) => {
@@ -522,14 +211,13 @@ const collageAuthSlice = createSlice({
       })
       .addCase(loginCollage.fulfilled, (state, action) => {
         // state.status = action.payload
+
         state.isLoggedIn = true;
         state.user = action.payload;
 
-        // Add any fetched posts to the array
-        console.log("fullfilled");
+        console.log(state.user);
       })
       .addCase(loginCollage.rejected, (state, action) => {
-        console.log(action.payload);
         alert(action.payload);
       })
       .addCase(updateCollege.pending, (state, action) => {
@@ -539,12 +227,14 @@ const collageAuthSlice = createSlice({
       .addCase(updateCollege.fulfilled, (state, action) => {
         // state.status = action.payload
         state.user = action.payload;
-
-        console.log("fullfilled");
+        window.location.reload(true);
+        console.log("update college fulfilled");
       })
       .addCase(updateCollege.rejected, (state, action) => {
         // console.log(action.payload);
         // window.alert(action.payload);
+        // window.location.reload(true);
+        console.log("rejected update profile");
       })
       .addCase(getCollege.pending, (state, action) => {
         state.status = "loading";
@@ -554,34 +244,34 @@ const collageAuthSlice = createSlice({
         // state.status = action.payload
         state.isLoggedIn = true;
         state.user = action.payload;
-        // window.location.reload(true);
+
         // Add any fetched posts to the array
-        console.log("fullfilled");
+        console.log("fullfilled get college");
       })
       .addCase(getCollege.rejected, (state, action) => {
         console.log(action.payload);
-        // window.location.reload(true);
 
         // window.alert(action.payload);
       })
       .addCase(updateAvatar.pending, (state, action) => {
         state.status = "loading";
-        console.log("pending");
+        console.log("pending avatar");
       })
       .addCase(updateAvatar.fulfilled, (state, action) => {
         // state.status = action.payload
 
         state.status = "success";
-        state.user = action.payload;
+        console.log(action);
+        // state.user = action.payload.college;
         state.uploadImg = true;
-        // state.user = action.payload;
+        state.user.avatar = action.payload.college.avatar;
 
         // getCollege();
         // Add any fetched posts to the array
-        console.log("fullfilled");
+        console.log("fullfilled avatar");
       })
       .addCase(updateAvatar.rejected, (state, action) => {
-        console.log(action.payload);
+        console.log("rejected avatar" + action.payload);
 
         // window.alert(action.payload);
       })
@@ -591,6 +281,8 @@ const collageAuthSlice = createSlice({
       })
       .addCase(logoutCollage.fulfilled, (state, action) => {
         // state.status = action.payload
+        localStorage.setItem("editable", false);
+        state.user = null;
         state.isLoggedIn = false;
 
         // Add any fetched posts to the array
