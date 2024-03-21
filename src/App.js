aba94c5d95bb8127387cfa4c8ceefbc0f2ed66a3
sidd333 <siddhantchtri@gmail.com>
2024-03-21 19:00:14 +0530

    fa fix

--- src/App.js
+++ src/App.js
@@ -122,7 +122,7 @@ export default function App() {
     //     scriptLoaded = false;
     //   }
     // };
-  }, [window.googleTranslateElementInit]);
+  }, []);
 
   return (
     <React.Fragment>
