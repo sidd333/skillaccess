ab938ce9e80a92941f72d232e7a2171b3ae8039f
dheeraj charan <dheerajcharan.dc@gmail.com>
2024-03-21 13:09:40 +0530

    qr code and otp done

---
+++ src/pages/collage/settings/SecondFAPage.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SecurityApp from "../../../components/collage/settings/security/SecondFA";
+
+const SecondFAPage = () => {
+  return (
+      <SecurityApp />
+  );
+};
+
+export default SecondFAPage;
--- src/pages/collage/settings/index.js
+++ src/pages/collage/settings/index.js
@@ -5,12 +5,16 @@ const SettingsPage = lazy(() => import("./SettingsPage.jsx"));
 const NotificationsPage = lazy(() => import("./NotificationsPage.jsx"));
 const ActivityPage = lazy(() => import("./ActivityPage.jsx"));
 const SecurityPage = lazy(() => import("./SecurityPage.jsx"));
+const SecurityAppPage = lazy(() => import("./SecurityAppPage.jsx"));
+const SecondFAPage = lazy(() => import("./SecondFAPage.jsx"));
 
 function SettingsRoute() {
   return (
     <Route path="collage/settings">
       <Route path="" element={<SettingsPage />} />
       <Route path="security" element={<SecurityPage />} />
+      <Route path="security/securityApp" element={<SecurityAppPage />} />
+      <Route path="security/secondFA" element={<SecondFAPage />} />
       <Route path="activity" element={<ActivityPage />} />
       <Route path="notifications" element={<NotificationsPage />} />
     </Route>
