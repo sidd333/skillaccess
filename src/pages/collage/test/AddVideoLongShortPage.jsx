6246d03c7c44f93e167d1e461a27740daada9360
AnkitaMalik22 <ankitamalik936@gmail.com>
2024-03-21 18:12:14 +0530

    fixed :  duplicate Translate Element

--- src/pages/collage/test/AddVideoLongShortPage.jsx
+++ src/pages/collage/test/AddVideoLongShortPage.jsx
@@ -5,6 +5,43 @@ import LongShortAnswer from "../../../components/collage/test/addVideo/LongShort
 import CollageLayout from "../../../layout/Collage";
 
 const AddVideoLongShortPage = () => {
+  React.useEffect(() => {
+   
+    let scriptLoaded = false;
+
+    let script = document.createElement("script");
+    const loadGoogleTranslateScript = () => {
+      if (!scriptLoaded) {
+        script.src =
+          "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+        script.async = true;
+        script.onload = () => {
+          window.googleTranslateElementInit = () => {
+            new window.google.translate.TranslateElement(
+              {
+                pageLanguage: "en",
+                includedLanguages: "en,hi,bn,ta,te,mr,gu,kn,ur,pa,ml,or", // Add more languages as needed
+              },
+              "google_translate_element"
+            );
+          };
+        };
+
+        document.body.appendChild(script);
+        scriptLoaded = true;
+      }
+    };
+    loadGoogleTranslateScript();
+
+
+  return () => {
+    // Clean up script when component unmounts
+    if (scriptLoaded) {
+      document.body.removeChild(script);
+      scriptLoaded = false;
+    }
+  };
+}, []);
   return (
     <CollageLayout>
       <LongShortAnswer />
--- src/pages/collage/test/AssessmentPage.jsx
+++ src/pages/collage/test/AssessmentPage.jsx
@@ -7,6 +7,43 @@ const Assessment = lazy(() =>
 const CollageLayout = lazy(() => import("../../../layout/Collage"));
 
 const AssessmentPage = () => {
+  React.useEffect(() => {
+   
+    let scriptLoaded = false;
+
+    let script = document.createElement("script");
+    const loadGoogleTranslateScript = () => {
+      if (!scriptLoaded) {
+        script.src =
+          "https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit";
+        script.async = true;
+        script.onload = () => {
+          window.googleTranslateElementInit = () => {
+            new window.google.translate.TranslateElement(
+              {
+                pageLanguage: "en",
+                includedLanguages: "en,hi,bn,ta,te,mr,gu,kn,ur,pa,ml,or", // Add more languages as needed
+              },
+              "google_translate_element"
+            );
+          };
+        };
+
+        document.body.appendChild(script);
+        scriptLoaded = true;
+      }
+    };
+    loadGoogleTranslateScript();
+
+
+  return () => {
+    // Clean up script when component unmounts
+    if (scriptLoaded) {
+      document.body.removeChild(script);
+      scriptLoaded = false;
+    }
+  };
+}, []);
   return (
     <ErrorBoundary FallbackComponent={Fallback} onReset={() => {}}>
       <Suspense fallback={<>loading</>}>
