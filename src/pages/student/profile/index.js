de68538d326c3fa6fa5cb38d5b344b6636bc294b
sidd333 <siddhantchtri@gmail.com>
2024-03-27 15:05:08 +0530

    profinal final

--- src/pages/student/profile/index.js
+++ src/pages/student/profile/index.js
@@ -2,10 +2,12 @@ import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import Profile from "./ProfilePage";
 
+const ProfileFinal = lazy(() => import("./ProfileFinalPage"));
 const ProfileRoutes = () => {
   return (
-    <Route>
-      <Route path="/student/profile" element={<Profile />} />
+    <Route path="/student/profile">
+      <Route path="" element={<Profile />} />
+      <Route path="final" element={<ProfileFinal />} />
     </Route>
   );
 };
